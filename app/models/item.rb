5d3e140adce1ea6cb353407f41a75654539d5270
Sofía Castillo <sofiaprogramadora@gmail.com>
2017-08-31 12:57:06 -0300

    Quinto commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
 	belongs_to :category
 	belongs_to :user, optional: true
+
+	#devolver todos aquellos que tienen tamaño X
+	scope :size_is, ->(a) { where("size = ?", a) }
+
+	#Crear un scope que devuelva los items que no tienen
+	#usuarios asignado.
+
+	scope :no_user, -> {where(user_id: nil)}
+
 end
